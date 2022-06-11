a2c1844ac96ea6c66bd9636efb789300ace1f0f4
trumpetYK <lim0386@gmail.com>
2022-06-11 23:16:34 +0900

    change

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+// const { isPromise } = require('util/types');
+// const socketIO = require('socket.io')(3000);
+
 var express = require('express');
 var app = express();
 var server = app.listen(process.env.PORT || 3000, listen);
@@ -10,11 +13,25 @@ function listen() {
 
 app.use(express.static('public'));
 
+// // on은 클라이언트에서 보낸 이벤트를 실행할 때 사용sudo
+// socketIO.on('connection', function(socket) {
+// 	console.log('Player Connected');
+//     // emit은 클라이언트에 이벤트를 보낼 때 사용
+//     var text1 = "내 딸 다경이를...";
+//     socketIO.emit('PlayerConnected');
+//     socketIO.emit('message', text1);
+//     socketIO.on('disconnect', function() {
+//         console.log('A Player disconnected');
+//     });
+// });
+
+
 var io = require('socket.io')(server);
 
 io.sockets.on('connection', function(socket) {
 
-  console.log("We have a new client: " + socket.id);
+  console.log('Player Connected');
+  socket.emit('PlayerConnected');
 
   socket.on('message', function(data) {
     //         console.log("Received: 'message'"+data);
