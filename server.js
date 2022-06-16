ae34600a7d4e5b1904faf28eac743193ecbc7f00
trumpetYK <lim0386@gmail.com>
2022-06-17 00:53:15 +0900

    ㅕㅔㅇㅁㅅㄷ ㄴㄷㄱㅍㄷㄱ

---
+++ server.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var app = express();
+var server = app.listen(process.env.PORT || 3000, listen);
+
+function listen() {
+  var host = server.address().address;
+  var port = server.address().port;
+  console.log('Example app listening at http://' + host + ':' + port);
+}
+
+app.use(express.static('public'));
+
+var io = require('socket.io')(server);
+
+io.sockets.on('connection', function(socket) {
+
+  console.log("We have a new client: " + socket.id);
+  console.log('Player Connected');
+  socket.emit('PlayerConnected');
+  socket.on('message', function(data) {
+  socket.broadcast.emit('message', data);
+  });
+
+  socket.on('disconnect', function() {
+    console.log("Client has disconnected");
+  });
+});
